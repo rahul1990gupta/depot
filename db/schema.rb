837bd486cc49a65f12e4e5494e7b380ee5a363df
Rahul Gupta <rahul1990gupta@gmail.com>
2024-12-30 11:14:46 +0530

    adds cart functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_28_141711) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_28_150544) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -21,6 +21,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_28_141711) do
     t.integer "cart_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity", default: 1
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
     t.index ["product_id"], name: "index_line_items_on_product_id"
   end
