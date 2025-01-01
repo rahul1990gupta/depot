75a1e461c43aadad8b2e0239c8328637ff973151
Rahul Gupta <rahul1990gupta@gmail.com>
2025-01-01 21:13:13 +0530

    finish adding payment processor

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,17 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Set localhost to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  # config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    user_name: "dave",
+    password: "secret",
+    enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
